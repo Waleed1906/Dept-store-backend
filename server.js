8f247d9af7de9b9888dee6860d26d85e0b28514b
abdullahrabi <abdullahrabi026@gmail.com>
2025-01-05 16:54:40 +0500

    fixes

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const mongoose = require ('mongoose');
 const dotenv = require ('dotenv');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
