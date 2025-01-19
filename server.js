c7016436c4ec7a898c7b5e1f9f1e62408a8a2e62
abdullahrabi <abdullahrabi026@gmail.com>
2025-01-20 03:57:42 +0500

    Testing fix Image Upload issue

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use("/images", express.static(uploadDir));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:5000/images/${req.file.filename}`,
+    image_url: `https://dept-store-backend-idke.vercel.app/images/${req.file.filename}`,
   });
 });
 
