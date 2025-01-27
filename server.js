7ca9835dcf309bbb048b360c3c17f2b87417de38
abdullahrabi <abdullahrabi026@gmail.com>
2025-01-28 01:31:06 +0500

    Added Again Cors policy

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const fs = require("fs");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2; // Import Cloudinary
 const user = require("./models/user");
-
+pp.use(cors({
+  origin: 'http://localhost:5173', // Allow requests from your frontend's URL
+  methods: 'GET,POST,PUT,DELETE',  // Specify the allowed HTTP methods
+  credentials: true,  // Allow cookies and headers
+  origin: 'http://localhost:3000',  
+}));
 // Initialize Express app
 const app = express();
 dotenv.config();
