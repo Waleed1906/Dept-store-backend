2270ed83936175960fb4d667b0445e59e3bda21f
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-22 21:28:21 +0500

    Fixing issues

--- routes/auth.js
+++ routes/auth.js
@@ -35,11 +35,12 @@ router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ success:false, message: 'Register First Kindly!' });
-    if (user) {
-      return res.json({ success:true, message: 'User Login successfully!' });
-    }
+    
 
     const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      return res.json({ success:true, message: 'User Login successfully!' });
+    }
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     // Generate JWT token
