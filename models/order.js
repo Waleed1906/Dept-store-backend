8f4b9e7890a9e3d3e687c95addf33a5070f21bf6
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-28 03:20:28 +0500

    testing solution

--- models/order.js
+++ models/order.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+  email: { type: String, required: true, trim: true },       
   userId: { type: String, required: true },
   fullName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true },
-  address: { type: String, required: true, trim: true },
   phoneNumber: { type: String, required: true, trim: true },
-  paymentMethod: { type: String, required: true, enum: ['Cash on Delivery', 'Card']},
-  paymentStatus: { type: String, required: true, enum: ['Pending', 'Completed', 'Failed'], default: 'Pending' },
+  address: { type: String, required: true, trim: true },
+  paymentMethod: { type: String, required: true, enum: ['Cash on Delivery', 'Card'] },
+  paymentStatus: { type: String, required: true, enum: ['Pending', 'Paid', 'Failed', 'Canceled'], default: 'Pending' },
   orderData: { type: Object, required: true },
-  total :{type:Number, required:true},
-  paymentIntentId: { type: String, required: true, unique:true }, 
+  total: { type: Number, required: true },
+  paymentIntentId: { type: String, required: true, unique: true },
   date: { type: Date, default: Date.now }
 });
 
